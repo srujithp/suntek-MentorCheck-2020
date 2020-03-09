079f2458a0c291437ce92670c17f66d4766792cc
Srujith Reddy Panuganti <55059610+srujithp@users.noreply.github.com>
2020-03-09 19:43:13 +0530

    Update OddOneOut.java

--- OddOneOut.java
+++ OddOneOut.java
@@ -1,3 +1,10 @@
+/* 6.Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+Example 1:
+Input: [2,2,3,2]
+Output: 3 */
+
 import java.util.*;
 import java.io.*;
 public class OddOneOut {
