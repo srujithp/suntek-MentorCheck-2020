6628d25962e82e397f94d28f740d166a749d4252
Srujith Reddy Panuganti <55059610+srujithp@users.noreply.github.com>
2020-03-12 23:25:50 +0530

    Update and rename XORResult.java to Prog1.java

--- Prog1.java
+++ Prog1.java
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class XORResult {
+public class Prog1 {
 
 	public static void main(String[] args) throws IOException {
 		
