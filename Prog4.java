ccd4eef94ba2de34a0961bda62bc845c0bbf525c
Srujith Reddy Panuganti <55059610+srujithp@users.noreply.github.com>
2020-03-12 23:25:04 +0530

    Update and rename StepsCount.java to Prog4.java

--- Prog4.java
+++ Prog4.java
@@ -16,7 +16,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class StepsCount {
+public class Prog4 {
 
 	public static void main(String[] args) throws IOException {
 		
